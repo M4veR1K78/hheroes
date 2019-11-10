8f3a657efc469ee0e9ee4cee9c151c22c1802f3f
bastien <bastien.tresse@gmail.com>
2019-11-10 17:06:06 +0100

    adding a new boss

--- src/main/config/database/hheroes-init.sql
+++ src/main/config/database/hheroes-init.sql
@@ -8,4 +8,5 @@ INSERT INTO hheroes.boss (id, libelle, orgasm, ego, x, d, nb_org, figure, world)
 INSERT INTO hheroes.boss (id, libelle, orgasm, ego, x, d, nb_org, figure, world) VALUES (8, 'Finalmecia', 13395, 6394.56, 0, 6394.56, 0, 12, '9');
 INSERT INTO hheroes.boss (id, libelle, orgasm, ego, x, d, nb_org, figure, world) VALUES (9, 'Roko Seiseï', 34003, 15704.8, 0, 15704.8, 0, 7, '10');
 INSERT INTO hheroes.boss (id, libelle, orgasm, ego, x, d, nb_org, figure, world) VALUES (10, 'Karole', 49504, 21622.4, 0, 21622.4, 0, 2, '11');
-INSERT INTO hheroes.boss (id, libelle, orgasm, ego, x, d, nb_org, figure, world) values (11, 'Equipage de Jackson', 57120, 23863.0, 0.0, 23863.0, 0, 11, '12');
+INSERT INTO hheroes.boss (id, libelle, orgasm, ego, x, d, nb_org, figure, world) VALUES (11, 'Equipage de Jackson', 57120, 23863.0, 0.0, 23863.0, 0, 11, '12');
+INSERT INTO hheroes.boss (id, libelle, orgasm, ego, x, d, nb_org, figure, world) VALUES (12, 'Sorcière Pandora', 84000, 36250.0, 0.0, 36250.0, 0, 11, '13');
